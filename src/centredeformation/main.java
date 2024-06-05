e64a89720af2efd0287b118af00fe32b6e8b4ad6
tomy0nEm <tomyninho972@gmail.com>
2024-06-05 12:56:33 +0200

    commit 3

--- src/centredeformation/main.java
+++ src/centredeformation/main.java
@@ -1,10 +1,19 @@
 package centredeformation;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		individu eleve1 = new Etudiant("Rinto","Thomas", 001, null, "Licence 3");
+		
+		
+		individu intervenant1 = new Enseignant("Menes","Pierre", 002,"Statistiques ",50.0);
+		
+		ArrayList<individu> listeIndividus = new ArrayList<individu>();
+		
+		
 	}
 
 }
